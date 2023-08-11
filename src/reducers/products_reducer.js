f196c6f696a3534025c7240c85c5aae5e5d1507b
yigitbaris <bariskaynakoglu01@gmail.com>
2023-08-11 17:11:46 +0300

    filtering and cart features added

--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -16,6 +16,7 @@ const products_reducer = (state, action) => {
   if (action.type === SIDEBAR_CLOSE) {
     return { ...state, isSidebarOpen: false }
   }
+
   if (action.type === GET_PRODUCTS_BEGIN) {
     return { ...state, products_loading: true }
   }
@@ -23,16 +24,17 @@ const products_reducer = (state, action) => {
     const featured_products = action.payload.filter(
       (product) => product.featured === true
     )
-    return { ...state, products_loading: false, featured_products }
-  }
-  if (action.type === GET_PRODUCTS_ERROR) {
     return {
       ...state,
       products_loading: false,
-      products_error: true,
+      products: action.payload,
+      featured_products,
     }
   }
-  if (action.type == GET_SINGLE_PRODUCT_BEGIN) {
+  if (action.type === GET_PRODUCTS_ERROR) {
+    return { ...state, products_loading: false, products_error: true }
+  }
+  if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
     return {
       ...state,
       single_product_loading: true,
