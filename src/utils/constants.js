b253f699f05e5e7836cc265b8c456cfe0e1973bd
yigitbaris <bariskaynakoglu01@gmail.com>
2024-07-18 16:13:41 +0300

    conflict fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -23,43 +23,22 @@ export const services = [
     id: 1,
     icon: <GiCompass />,
     title: 'mission',
-<<<<<<< HEAD
     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
-=======
-    text:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
->>>>>>> d2de8d86ac6f99408994203d8f9be34c6b753915
   },
   {
     id: 2,
     icon: <GiDiamondHard />,
     title: 'vision',
-<<<<<<< HEAD
     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
-=======
-    text:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
->>>>>>> d2de8d86ac6f99408994203d8f9be34c6b753915
   },
   {
     id: 3,
     icon: <GiStabbedNote />,
     title: 'history',
-<<<<<<< HEAD
     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
-  },
-]
-
-export const products_url = 'https://www.course-api.com/react-store-products'
-
-export const single_product_url = `https://wwww.course-api.com/react-store-single-product?id=`
-=======
-    text:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptates, ea. Perferendis corrupti reiciendis nesciunt rerum velit autem unde numquam nisi',
   },
 ]
 
 export const products_url = 'https://course-api.com/react-store-products'
 
 export const single_product_url = `https://course-api.com/react-store-single-product?id=`
->>>>>>> d2de8d86ac6f99408994203d8f9be34c6b753915
